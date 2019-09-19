95951ad28a9f26c7a0765a937662cd34690b3e2f
Nicolas Aldeco <nico.aldeco@gmail.com>
2019-09-19 12:18:56 -0300

    to listo

--- scripts_TP2/ej4.sh
+++ scripts_TP2/ej4.sh
@@ -1,6 +1,24 @@
-#!bin/bash
+#!/bin/bash
 
 echo SUMADOR: Suma y Resta de numeros enteros
 echo Ingrese su ecuacion
 read EQ
-#TERMINIAR THIS SHIT
+RES=0
+
+for letter in $EQ + 0
+do
+    if echo $letter | egrep -q '^[0-9]+$'; then
+    NUM=$letter
+    else
+    case "$letter" in
+    "+") let RES=RES+NUM
+    ;;
+    "-") let RES=RES-NUM
+    ;;
+    *) echo Opcion incorrecta, comienze de nuevo
+    exit
+    esac
+    fi
+done
+
+echo El resutado es $RES
--- scripts_TP3/ej5.sh
+++ scripts_TP3/ej5.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
-#DO THIS SHIT MEEEEN#
+
+DIR=/bin/copiabin
+UBI=$1/*
+
+if [ -z $1 ];then
+echo Tienes que ingresar el directorio que quieres hacer copia
+exit
+fi
+
+if [ -e $DIR ];then
+echo El directorio ya existe
+else
+echo Creando directorio copiabin
+sudo mkdir $DIR
+fi
+
+for f in $UBI
+do
+    if [ -x $f ];then
+        sudo cp -a $f $DIR
+        filename=`basename $f`
+        echo El archivo $filename se movio a $DIR
+        let MOVE=MOVE+1
+    fi
+done
+
+echo Se movieron $MOVE archivos a $DIR
